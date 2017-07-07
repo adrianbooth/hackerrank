5f127a3280e5b11c3868887e0e647b996dfe435e
Adrian Booth <adz001@msn.com>
2017-07-07 23:03:29 +0100

    Birthday Cake Candles

--- src/ConsoleApp2/ConsoleApp2/Program.cs
+++ src/ConsoleApp2/ConsoleApp2/Program.cs
@@ -5,17 +5,19 @@ namespace ConsoleApp2
 {
     class Program
     {
+        static int birthdayCakeCandles(int n, int[] ar)
+        {
+            var highestCandle = ar.Max();
+            return ar.Where(e => e == highestCandle).Count();
+        }
+
         static void Main(String[] args)
         {
-            string[] arr_temp = Console.ReadLine().Split(' ');
-            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
-
-            var orderedArray = arr.OrderBy(e => e);
-            long lowestSum = orderedArray.Select(e => (long)e).Take(4).Sum();
-            long highestSum = orderedArray.Select(e => (long)e).Skip(1).Take(4).Sum();
-
-            Console.WriteLine($"{lowestSum} {highestSum}");
-
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = birthdayCakeCandles(n, ar);
+            Console.WriteLine(result);
             Console.ReadLine();
         }
     }
