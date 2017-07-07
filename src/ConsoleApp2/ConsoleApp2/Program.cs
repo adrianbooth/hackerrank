8d1bcbdc8dedef2afd2745ff98a2e0dbc1a20407
Adrian Booth <adz001@msn.com>
2017-07-07 22:32:42 +0100

    Plus Minus problem
    Also removed visual studio build files and added an approproate git ignore for the solution type

--- src/ConsoleApp2/ConsoleApp2/Program.cs
+++ src/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp2
 {
@@ -11,38 +8,14 @@ namespace ConsoleApp2
         static void Main(String[] args)
         {
             int n = Convert.ToInt32(Console.ReadLine());
-            int[][] a = new int[n][];
-            for (int a_i = 0; a_i < n; a_i++)
-            {
-                string[] a_temp = Console.ReadLine().Split(' ');
-                a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
-            }
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            decimal itemCount = n;
 
-            var primaryDiagonal = SumPrimaryDiagonal(a, n);
-            var secondaryDiagonal = SumSecondaryDiagonal(a, n);
-
-            Console.WriteLine(Math.Abs(primaryDiagonal - secondaryDiagonal));
-        }
-
-        private static int SumPrimaryDiagonal(int[][] matrix, int length)
-        {
-            int sum = 0;
-
-            for (int i = 0; i < length; i++)
-            {
-                sum += matrix[i][i];
-            }
-            return sum;
-        }
-        private static int SumSecondaryDiagonal(int[][] matrix, int length)
-        {
-            int sum = 0;
-
-            for (int i = 0; i < length; i++)
-            {
-                sum += matrix[i][length - (1 + i)];
-            }
-            return sum;
+            Console.WriteLine(Math.Round(arr.Where(e => e > 0).Count() / itemCount, 6));
+            Console.WriteLine(Math.Round(arr.Where(e => e < 0).Count() / itemCount, 6));
+            Console.WriteLine(Math.Round(arr.Where(e => e == 0).Count() / itemCount, 6));
+            Console.ReadLine();
         }
     }
 }
